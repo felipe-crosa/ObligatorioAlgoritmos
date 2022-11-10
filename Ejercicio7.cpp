ee1921168d62b711b3ab3ab43aa00bc3aa9ddfb7
felipe-crosa <felipe@lightit.io>
2022-11-10 09:18:40 -0300

    terminar ejercicio 7

--- Ejercicio7.cpp
+++ Ejercicio7.cpp
@@ -4,14 +4,8 @@ using namespace std;
 
 
 int MaximaSumatoria(Lista<int>* pila1, Lista<int>* pila2, Lista<int>* pila3, int suma1, int suma2, int suma3){
-    int ret = 0;
-    bool salir = false;
-    while(!pila1->esVacia() && !pila2->esVacia() && !pila3->esVacia() && !salir){
-        
-        if(suma1 == suma2 && suma1 == suma3){
-            ret = suma1;
-            salir = true;
-        }
+    while(!pila1->esVacia() && !pila2->esVacia() && !pila3->esVacia()){
+        if(suma1 == suma2 && suma1 == suma3) return suma1;
 
         if(suma1 >= suma2 && suma1 >= suma3){
             suma1 -= pila1->obtenerPpio();
@@ -26,41 +20,35 @@ int MaximaSumatoria(Lista<int>* pila1, Lista<int>* pila2, Lista<int>* pila3, int
             pila3->borrarPpio();
         }
     }
-    return ret;
+    return 0;
 }
 
+int cargarPila(Lista<int>* pila , int largo) {
+    int suma = 0;
+    for(int i=0; i<largo; i++){
+        int n;
+        cin >> n;
+        suma += n;
+        pila->insertarFin(n);
+    }
+    return suma;
+}
 
 int main(){
     int largo1, largo2, largo3;
-    int suma1 = 0;
-    int suma2 = 0;
-    int suma3 = 0;
+
     cin >> largo1;
     Lista<int>* pila1 = new Lista<int>();
-    for(int i=0; i<largo1; i++){
-        int n;
-        cin >> n;
-        suma1 += n;
-        pila1->insertarFin(n);
-    }
+    int suma1 = cargarPila(pila1, largo1);
+  
 
     cin >> largo2;
     Lista<int>* pila2 = new Lista<int>();
-    for(int i=0; i<largo2; i++){
-        int m;
-        cin >> m;
-        suma2 += m;
-        pila2->insertarFin(m);
-    }
+    int suma2 = cargarPila(pila2, largo2);
 
     cin >> largo3;
     Lista<int>* pila3 = new Lista<int>();
-    for(int i=0; i<largo3; i++){
-        int p;
-        cin >> p;
-        suma3 += p;
-        pila3->insertarFin(p);
-    }
+    int suma3 = cargarPila(pila3, largo3);
 
     cout << MaximaSumatoria(pila1, pila2, pila3, suma1, suma2, suma3) << endl;
     delete pila1;
